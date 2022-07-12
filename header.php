ec4fee671f55a0ac4edf151fbee3d36427cdc61d
kdslots777 <sonickorners@gmail.com>
2022-07-12 16:52:12 +0700

    First Commit

---
+++ header.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="preload" as="image" href="./assets/img/logo-splash-loading.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta content='https://www.google.com/q=slot-games/page=1' name='page google.com' />
+  <meta content='https://www.google.co.id/q=slot-games/page=1' name='page google.co.id' />
+  <title>9NAGAPOKER: Situs Agen Judi IDN Poker Online Uang Asli Terpercaya Indonesia</title>
+  <link rel="shortcut icon" type="image/x-icon" href="./assets/images/favicon.ico?v=5.7" />
+  <link rel="apple-touch-icon" href="./assets/images/favicon.ico?v=5.7">
+
+  <meta name="description" content="9NAGAPOKER adalah situs agen judi idn poker online uang asli terpercaya Indonesia di tahun 2022 Berlisensi resmi, Link Alternatif Poker - 9NagaPoker - Menyediakan Link Alternatif Poker dan Prediksi Permainan poker online terpercaya indonesia" />
+  <meta name="keywords" content="poker online, judi online, situs judi online, 9nagapoker, Link Alternatif, Situs Alternatif Poker Online, URL Alternatif Poker, GudangPoker,Dewapoker,9nagapoker,miyabipoker,kumpulan link alternatif poker 2022,lidewapoker, Link Alternatif Terupdate." />
+  <meta name="notif-language" content="ID">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="redirect_method" content="self" />
+  <meta name="geo.placename" content="Indonesia">
+  <meta name="geo.country" content="id">
+  <meta name="geo.region" content="ID-JK" />
+  <meta name="tng.nation" content="Indonesia">
+  <meta name="language" content="ID" />
+  <meta name="rating" content="general">
+  <meta name="distribution" content="global">
+  <meta name="author" content="kdslot777.com">
+  <meta name="publisher" content="9nagapoker by 999naga">
+  <meta name="copyright" content="Copyright 2019 @9nagapoker.com">
+  <meta name="DC.Creator" content="kdslot777.com">
+  <meta name="DC.Date.LastModified" scheme="ISO 31-1" content="2018-10-12">
+  <meta name="DC.Identifier" content="9nagapoker.com">
+  <meta name="DC.Publisher" content="9nagapoker by 999naga">
+  <meta property="og:image" content="./assets/images/favicon.png?v=2.3.0" />
+  <meta name="DC.Language" scheme="ISO639-2" content="id">
+  <link rel="canonical" href="./">
+  <meta name="search engines" content="Aeiwi, Alexa, AllTheWeb, AltaVista, AOL Netfind, Anzwers, 
+Canada, DirectHit, EuroSeek, Excite, Overture, Go, Google, HotBot, InfoMak, Kanoodle, Lycos, MasterSite, National Directory, Northern Light, SearchIt, SimpleSearch, WebsMostLinked, WebTop, What-U-Seek, AOL, Yahoo, WebCrawler, Infoseek, Excite, Magellan, LookSmart, bing, CNET, Googlebot">
+
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+</head>
+
+<body>
+  <?php
+  session_start();
+  ?>
+
+  <!-- NAVBAR -->
+  <div class="navnav sticky-top">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-ny">
+        <a class="navbar-brand" href="./"><img src="./assets/images/9nagapokerlogo.png" class="logo"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <br /><a data-toggle="modal" data-target="#login" onclick="window.location.href='#'"><svg aria-hidden="true" class="clssvg" height="35" preserveaspectratio="xMidYMid meet" role="img" viewbox="0 0 20 20" width="35" xmlns="http://www.w3.org/2000/svg">
+            <path d="M15 9h-1V6c0-2.2-1.8-4-4-4S6 3.8 6 6v3H5c-.5 0-1 .5-1 1v7c0 .5.5 1 1 1h10c.5 0 1-.5 1-1v-7c0-.5-.5-1-1-1zm-4 7H9l.4-2.2c-.5-.2-.9-.8-.9-1.3c0-.8.7-1.5 1.5-1.5s1.5.7 1.5 1.5c0 .6-.3 1.1-.9 1.3L11 16zm1-7H8V6c0-1.1.9-2 2-2s2 .9 2 2v3z" fill="white"></path>
+          </svg></a>
+        <div class="collapse navbar-collapse" id="navbarScroll">
+          <ul class="navbar-nav mr-auto my-2 my-lg-0 navbar-nav-scroll" style="max-height: 100px;">
+            <li class="nav-item active">
+              <a class="nav-link" href="./poker.html"><img src="./assets/images/nav/idnplay.webp" class="navbar-icon">Poker<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./idnlive.html"><img src="./assets/images/nav/sportsbook.webp" class="navbar-icon ">Idnlive</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./slots.html"><img src="./assets/images/nav/slots.webp" class="navbar-icon">Slots</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./togel.html"><img src="./assets/images/nav/lottery.webp" class="navbar-icon ">Togel</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./casino.html"><img src="./assets/images/nav/casino.webp" class="navbar-icon">Casino</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./sportsbook.html"><img src="./assets/images/nav/sportsbook.webp" class="navbar-icon ">Sportsbook</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./esport.html"><img src="./assets/images/nav/esport.webp" class="navbar-icon ">ESports</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                <img src="./assets/images/nav/others.png" class="navbar-icon">Link alt</a>
+              <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                <li><a class="dropdown-item" href="./esport.html">999naga.click</a></li>
+                <li><a class="dropdown-item" href="./fishinggame.html">999naga.us</a></li>
+                <li><a class="dropdown-item" href="./roulette.html">9nagapoker.com</a></li>
+                <li><a class="dropdown-item" href="./baccarat.html">999naga.pro</a></li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./promo.html"><img src="./assets/images/nav/promotion.webp" class="navbar-icon ">Promo</a>
+            </li>
+
+          </ul>
+
+          <form>
+            <div class="form-row align-items-center">
+
+              <a id="btnLogin" class="btn-login btn btn-success btn-type2-secondary" onclick="window.location.href='#'" data-toggle="modal" data-target="#login" class="text">Login</a>
+              <a id="btnSignup" class="btn-login btn btn-success btn-type2-secondary" onclick="window.location.href='./process-signup'" tabindex="4">Daftar</a>
+              <a href="./process-reset" class="text-c-i-white">Lupa Kata Sandi ?</a>
+            </div>
+          </form>
+        </div>
+      </nav>
+    </div>
+  </div>
+  <div class="content__running-text">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 content__running-text__msg">
+          <label class="content__running-text__msg__label">INFO TERBARU :</label>
+          <div class="content__running-text__msg__marquee">
+            <div class="content__running-text__msg__marquee__marquee">
+              <div id="runningText">
+                <div style="width: 10000px; transform: translateX(893.5px); loop: 10 animation: 200.44s linear 0s infinite normal none running marqueeAnimation-9557283;" class="js-marquee-wrapper">
+                  <div class="js-marquee" style="margin-right: 0px; float: left;color:chocolate">Selamat datang di 9NAGAPOKER.COM, Mohon perhatian no rek tujuan, silakan cek ke dalam Menu Deposit sebelum melakukan transfer uang. Terima kasih. Alter: 999naga.pro</a></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
